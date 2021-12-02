cab1190690189eda8cc900114c085f3e8473437e
황두리 <hwangduli0515@gmail.com>
2021-12-02 22:01:48 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Main {
+
+    public int solution(int n, int[][] arr) {
+
+        int answer = 0;
+        int sum1, sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println(solution.solution(n, arr));
+
+    }
+}
+
+
