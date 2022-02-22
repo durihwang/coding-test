b59635995b67651d880546f04bfea3f5a799d472
황두리 <hwangduli0515@gmail.com>
2022-02-22 23:32:51 +0900

    dfs bfs 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -19,39 +20,38 @@ class Main {
         }
     }
 
-    static int[][] graph;
-    static int[] ch;
-    static int[] ch2;
     static int n, m, k, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int ch[], dis[];
+    static int ch2[];
 
     public void solution() {
-        dfs(k);
+        ch2[1] = 1;
+        dfs(1);
         System.out.println();
-        bfs(k);
+        bfs(1);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + ": " + dis[i]);
+        }
+
+        System.out.println(answer);
     }
 
     public int bfs(int v) {
 
-        // 큐를 선언해준다.
         Queue<Integer> q = new LinkedList<>();
-        // 처음 시작되는 v값을 넣어준다.
+        ch[v] = 1;
+        dis[v] = 0;
         q.offer(v);
 
-        // 큐가 비어있을 떄까지 while문을 돌린다.
         while (!q.isEmpty()) {
-            // 제일 위에 큐를 꺼낸다.
-            Integer p = q.poll();
-            // 큐를 출력한다.
-            System.out.print(p + " ");
-
-            // 입력된 간선의 최대수만큼 for문을 돌려준다.
-            for (int i = 1; i <= n; i++) {
-                // 입력된 값이 존재하고, 방문하지 않았는지 체크해준다.
-                if ((graph[p][i] == 1 || graph[i][p] == 1) && ch2[i] == 0) {
-                    // 제일 처음 방문 표시를 한다.
-                    ch2[i] = 1;
-                    // 꺼낸 큐에서 이동된 값이 i 이므로 i를 큐에 넣어준다.
-                    q.offer(i);
+            int cv = q.poll();
+            for (int nv : graph.get(cv)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    q.offer(nv);
+                    dis[nv] = dis[cv] + 1;
                 }
             }
         }
@@ -61,17 +61,15 @@ class Main {
 
     public int dfs(int v) {
 
-        System.out.print(v + " ");
-
-        // 정점의 개수만큼 for문을 돌면서 모든 경우의 수를 탐색한다.
-        for (int i = 1; i <= n; i++) {
-            // 맨처음이면 v는 k일 것이고 1에서 1,2,3,4,5를 돌아서 갈 수 있는 길을 탐색한다.
-            // 1은 자기자신이니까 안되고 2부터 도는데 ch값이 1이면 이미 갔던 길이거나 못가는 길이므로 이것도 제외한다.
-            if ((graph[v][i] == 1 || graph[i][v] == 1) && ch[i] == 0) {
-                // 모든 경우의 수를 따질 때는 항상 먼처 이 길을 지나갔다는 체크를 해준다.
-                ch[i] = 1;
-                // 위 조건을 모두 만족하면 해당 정점에서 다시 재귀함수를 돌려준다.
-                dfs(i);
+        if (n == v) {
+            answer++;
+        } else {
+            for (int nv : graph.get(v)) {
+                if (ch2[nv] == 0) {
+                    ch2[nv] = 1;
+                    dfs(nv);
+                    ch2[nv] = 0;
+                }
             }
         }
 
@@ -95,18 +93,21 @@ class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        graph = new int[n + 1][n + 1];
+//        k = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
         ch = new int[n + 1];
         ch2 = new int[n + 1];
+        dis = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
         for (int i = 0; i < m; i++) {
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st2.nextToken());
             int b = Integer.parseInt(st2.nextToken());
-            graph[a][b] = 1;
+            graph.get(a).add(b);
         }
-        ch[k] = 1;
-        ch2[k] = 1;
+
         solution.solution();
     }
 }
---
+++ src/PGM01.java
@@ -0,0 +1,34 @@
+public class PGM01 {
+
+    static int ch[];
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        ch = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (ch[i] == 0) {
+                dfs(i, n, computers);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public int dfs(int i, int n, int[][] computers) {
+        ch[i] = 1;
+        for (int j = 0; j < computers.length; j++) {
+            if (i != j && ch[j] == 0 && computers[i][j] == 1) {
+                dfs(j, n, computers);
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        PGM01 pgm01 = new PGM01();
+//        int[][] arr = new int[][]{{1,1,0}, {1,1,0}, {0,0,1}};
+        int[][] arr = new int[][]{{1,1,0}, {1,1,1}, {0,1,1}};
+        System.out.println(pgm01.solution(3, arr));;
+    }
+}
