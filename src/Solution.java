5e1b63116a5842d8580f513af9b173efabfd644d
황두리 <hwangduli0515@gmail.com>
2022-06-13 21:34:05 +0900

    프로그래머스 문제풀이

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
 
-    public int solution(int[] rooms) {
-        int answer = 0;
-        int swap = 0;
-        for (int i = 0; i < rooms.length; i++) {
+    public String[] solution(String[][] booked, String[][] unbooked) {
 
-            int room = i + 1;
-            int key = rooms[i];
-            if (room == key) {
-                answer++;
-            } else if (room > key) {
-                answer++;
+        ArrayList<String> answer = new ArrayList<>();
+        int time;
+        int bIndex = 0;
+        int uIndex = 0;
+
+        if (booked.length == 0) {
+            for (String[] s : unbooked) {
+                answer.add(s[1]);
+            }
+            return answer.toArray(String[]::new);
+        } else {
+            time = Math.min(convertTime(booked[bIndex][0]), convertTime(unbooked[uIndex][0]));
+        }
+
+        while (bIndex < booked.length || uIndex < unbooked.length) {
+            if (bIndex < booked.length && time >= convertTime(booked[bIndex][0])) {
+                time = addTime(booked[bIndex][0]);
+                answer.add(booked[bIndex][1]);
+                bIndex++;
+            } else if (uIndex < unbooked.length && time >= convertTime(unbooked[uIndex][0])){
+                time = addTime(unbooked[uIndex][0]);
+                answer.add(unbooked[uIndex][1]);
+                uIndex++;
+            } else if (bIndex < booked.length) {
+                time = addTime(booked[bIndex][0]);
+                answer.add(booked[bIndex][1]);
+                bIndex++;
+            } else {
+                time = addTime(unbooked[uIndex][0]);
+                answer.add(unbooked[uIndex][1]);
+                uIndex++;
             }
         }
 
-        return answer;
+        return answer.toArray(String[]::new);
+    }
+
+    private int addTime(String time) {
+        String[] splitTime = time.split(":");
+        int hour = Integer.parseInt(splitTime[0]);
+        int min = Integer.parseInt(splitTime[1]) + 10;
+        if (min >= 60) {
+            hour++;
+            min -= 60;
+        }
+
+        String resultTime = hour + String.format("%02d", min);
+        return Integer.parseInt(resultTime);
+    }
+
+    private int convertTime(String time) {
+        String replaceTime = time.replace(":", "");
+        return Integer.parseInt(replaceTime);
     }
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] s = {3,1,2,4};
-//        int[] s = {2,3,4,5,1};
-//        int[] s = {1,2,3,4,5,6};
-        System.out.println(solution.solution(s));
+        String[][] booked = {{"09:55","hae"}, {"10:05","jee"}};
+        String[][] unbooked = {{"10:04","hee"}, {"14:07","eom"}};
+
+        System.out.println(Arrays.toString(solution.solution(booked, unbooked)));
     }
 }
