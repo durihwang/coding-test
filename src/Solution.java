20736c704e99774937a8e96173666549d7ed3499
황두리 <hwangduli0515@gmail.com>
2022-06-28 23:29:22 +0900

    LeetCode 문제풀이

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Solution {
 
-    public String longestCommonPrefix(String[] strs) {
+    public static class ListNode {
 
-        String answer = "";
-        int start = 0;
-        int end = 0;
+        int val;
+        ListNode next;
 
-        while (start < strs.length && end < strs.length) {
-            for (int i = 0; i < strs.length - 1; i++) {
-                for (int j = 0; j < strs[i].length(); j++) {
-                    System.out.println(strs[i].charAt(j));
-                }
-            }
+        ListNode() {
         }
 
+        ListNode(int val) {
+            this.val = val;
+        }
 
-        return answer;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] s = {"flower", "flow", "flight"};
-        System.out.println(solution.longestCommonPrefix(s));
+        ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        System.out.println(solution.mergeTwoLists(listNode1, listNode2));
+        ListNode listNode = solution.mergeTwoLists(listNode1, listNode2);
     }
 }
