9edd1ae7694d0f6a25bf7dce92bacfb01fe0ca47
황두리 <hdr@gabia.com>
2022-06-30 17:58:02 +0900

    LeetCode -> Longest Substring Without Repeating Characters

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class Solution {
 
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public int lengthOfLongestSubstring(String s) {
 
-        ArrayList<Integer> answer = new ArrayList<>();
+        if (s.length() == 0) {
+            return 0;
+        }
 
-        int length = matrix[0].length * matrix.length;
-        int xStart = 0;
-        int xEnd = matrix[0].length - 1;
-        int yStart = 0;
-        int yENd = matrix.length - 1;
+        HashSet<Character> compare = new HashSet<>();
+        int answer = 1;
+        int i = 0;
+        int j = 1;
 
-        while (answer.size() != length) {
-
-            // 동
-            for (int i = xStart; i <= xEnd && answer.size() < length; i++) {
-                answer.add(matrix[xStart][i]);
+        while (i < s.length() && j < s.length()) {
+            if (!compare.contains(s.charAt(j)) && s.charAt(i) != s.charAt(j)) {
+                compare.add(s.charAt(j));
+                answer = Math.max(answer, j - i + 1);
+                j++;
+            } else {
+                compare = new HashSet<>();
+                i++;
+                j = i + 1;
             }
-            yStart++;
-
-            // 남
-            for (int i = yStart; i <= yENd && answer.size() < length; i++) {
-                answer.add(matrix[i][xEnd]);
-            }
-            xEnd--;
-
-            // 서
-            for (int i = xEnd; i >= xStart && answer.size() < length; i--) {
-                answer.add(matrix[yENd][i]);
-            }
-            yENd--;
-
-            // 북
-            for (int i = yENd; i >= yStart && answer.size() < length; i--) {
-                answer.add(matrix[i][xStart]);
-            }
-            xStart++;
         }
 
         return answer;
@@ -51,6 +36,6 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        System.out.println(solution.spiralOrder(matrix));
+        System.out.println(solution.lengthOfLongestSubstring(""));
     }
 }
