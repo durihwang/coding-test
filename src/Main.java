00aed7020d54983a17492c408b86e2407d81b2ce
황두리 <hwangduli0515@gmail.com>
2022-07-27 20:19:46 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,32 @@ import java.util.*;
 
 class Main {
 
-    public int removeDuplicates(int[] nums) {
+    public int solution(String s, String t) {
 
-        HashSet<Integer> hashSet = new LinkedHashSet<>();
+        int answer = 0;
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
 
-        for (int s : nums) {
-            hashSet.add(s);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < t.length(); j++) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                    answer = Math.max(answer, dp[i + 1][j + 1]);
+                }
+            }
         }
 
-        int i = 0;
-        for (int s : hashSet) {
-            nums[i] = s;
-            i++;
-        }
-
-        return hashSet.size();
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
+
         Main solution = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] nums = {-3,-1,0,0,0,3,3};
-        System.out.println(solution.removeDuplicates(nums));
+        String s = br.readLine();
+        String t = br.readLine();
+
+        System.out.println(solution.solution(s, t));
+
     }
 }
