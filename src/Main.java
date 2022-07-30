792ef8f402df25c7f09a2e77eb7915419f1938ec
황두리 <hwangduli0515@gmail.com>
2022-07-30 20:22:01 +0900

    백준 11656 접미사 배열

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,18 @@ import java.util.*;
 
 class Main {
 
-    public void solution(String[] s) {
+    public void solution(String s) {
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
-        for (int i = 0; i < s.length; i++) {
-            int result = checkPalindrome(0, s[i].length() - 1, 0, s[i]);
-
-            if (result == 2) {
-
-            } else {
-                list.add(result);
-            }
+        for (int i = 0; i < s.length(); i++) {
+            list.add(s.substring(i));
         }
 
-        for (int x : list) {
-            System.out.println(x);
-        }
-    }
+        Collections.sort(list);
 
-    private int checkPalindrome(int left, int right, int check, String s) {
-
-        int pseudo = 0;
-
-        while (left < right) {
-
-            if (s.charAt(left) != s.charAt(right)) {
-                check++;
-                pseudo = Math.min(checkPalindrome(left + 1, right, check, s),
-                    checkPalindrome(left, right - 1, check, s));
-            }
-            left++;
-            right--;
-        }
-
-        System.out.println("pseudo = " + pseudo);
-
-        if (check >= 2) {
-            return 2;
-        } else if (check == 1){
-            return 1;
-        } else {
-            return 0;
+        for (String t : list) {
+            System.out.println(t);
         }
     }
 
@@ -55,11 +25,13 @@ class Main {
         Main solution = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        /*int n = Integer.parseInt(br.readLine());
         String[] s = new String[n];
         for (int i = 0; i < n; i++) {
             s[i] = br.readLine();
-        }
+        }*/
+
+        String s = br.readLine();
 
         solution.solution(s);
 
