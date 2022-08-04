25cbc7c60159adcf1df4f4c6168625c22027b0ea
황두리 <hdr@gabia.com>
2022-08-04 18:05:18 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,16 @@ class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int i1 = Integer.parseInt(br.readLine());
-        int n = 0;
-        String s = br.readLine();
-        for (int i = 0; i < s.length(); i++) {
-            n += Character.getNumericValue(s.charAt(i));
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            StringBuilder append = stringBuilder.append("*");
+            String format = String.format("%" + n + "S", append);
+            StringBuilder reverse = new StringBuilder(format).reverse();
+            String convert = format + reverse.substring(1);
+            System.out.println(convert);
         }
-        System.out.println(n);
     }
 }
