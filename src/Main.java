9b106efb6fb5ae00a9434e91342e706ecc1d8794
황두리 <hwangduli0515@gmail.com>
2022-08-29 01:05:55 +0900

    java 14889 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,91 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 class Main {
 
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int answer = 0;
+    static int[][] s;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int answer = 0;
-        int sum;
-        int[][] paper = new int[n][m];
-
+        int n = Integer.parseInt(br.readLine());
+        s = new int[n][n];
         for (int i = 0; i < n; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                paper[i][j] = Integer.parseInt(st2.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                s[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                // 일자
-                if (j + 4 < m) {
-                    sum = paper[i][j] + paper[i][j + 1] + paper[i][j + 2] + paper[i][j + 3];
-                    answer = Math.max(answer, 3);
-                }
-
-                // 네모
-
-                // ㄱ자
-
-                // ㄹ자
-
-                // ㅗ자
-            }
-        }
+        answer = go(0, new ArrayList<>(), new ArrayList<>(), n);
 
         System.out.println(answer);
-
     }
+
+    static int go(int index, ArrayList<Integer> first, ArrayList<Integer> second, int n) {
+
+        // 인덱스와 주어진 n값이 같은 경우
+        if (index == n) {
+            if (first.size() != n / 2) {
+                return -1;
+            }
+            if (second.size() != n / 2) {
+                return -1;
+            }
+
+            int t1 = 0;
+            int t2 = 0;
+
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = 0; j < n / 2; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    t1 += s[first.get(i)][first.get(j)];
+                    t2 += s[second.get(i)][second.get(j)];
+                }
+            }
+
+            System.out.println(t1);
+            System.out.println(t2);
+            System.out.println();
+            return Math.abs(t1 - t2);
+
+        } else {
+
+            if (first.size() > n / 2) {
+                return -1;
+            }
+            if (second.size() > n / 2) {
+                return -1;
+            }
+            int ans = -1;
+
+            // 첫번째 팀인 경우
+            first.add(index);
+            int team1 = go(index + 1, first, second, n);
+            if (team1 != -1 && ans > team1) {
+                ans = team1;
+            }
+            first.remove(first.size() - 1);
+
+            // 두번째 팀인 경우
+            second.add(index);
+            int team2 = go(index + 1, first, second, n);
+            if (team2 != -1 && ans > team2) {
+                ans = team2;
+            }
+            second.remove(second.size() - 1);
+
+            return ans;
+        }
+   }
+
+
 }
