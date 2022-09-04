dab69c04e8f6ff86708caa7749491621f878b337
황두리 <hwangduli0515@gmail.com>
2022-09-04 23:20:26 +0900

    백준 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -8,62 +8,60 @@ import java.util.*;
 
 class Main {
 
-    static String[] k;
-    static boolean[] check;
-    static ArrayList<String> answer = new ArrayList<>();
-
+    static int n;
+    static int[] answer;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        k = new String[n + 1];
-        check = new boolean[10];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            k[i] = st.nextToken();
+        n = Integer.parseInt(br.readLine());
+        answer = new int[n];
+        for (int i = 1; i <= n; i++) {
+            answer[i - 1] = i;
         }
 
-        go(0, "", n);
-
-        Collections.sort(answer);
-        System.out.println(answer.get(answer.size() - 1));
-        System.out.println(answer.get(0));
+        do {
+            for (int x : answer) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } while (go());
 
     }
 
-    static void go(int level, String nums, int n) {
-        if (level == n + 1) {
-            if (ok(nums, n)) {
-                answer.add(nums);
-            }
-        } else {
-            for (int i = 0; i <= 9; i++) {
-                if (!check[i]) {
-                    check[i] = true;
-                    go(level + 1, nums + i, n);
-                    check[i] = false;
-                }
-            }
+    static boolean go() {
+        // 1. 오른쪽부터 i와 i-1을 비교하여 i-1이 더 작을때까지 비교한다. (i찾기)
+        int i = n - 1;
+        while (i > 0 && answer[i] <= answer[i - 1]) {
+            i--;
         }
-    }
 
-    static boolean ok(String nums, int n) {
-        for (int i = 0; i < n; i++) {
-            int n1 = Character.getNumericValue(nums.charAt(i));
-            int n2 = Character.getNumericValue(nums.charAt(i + 1));
-            if (k[i].equals("<")) {
-                if (n1 > n2) {
-                    return false;
-                }
-            } else if (k[i].equals(">")) {
-                if (n1 < n2) {
-                    return false;
-                }
-            }
+        // 마지막 순열이라면 -1 리턴
+        if (i <= 0) {
+            return false;
+        }
+
+        // 2. i를 찾았으면 제일 끝에서부터 i-1보다 큰 값을 찾는다.
+        // i 부터 끝까지지 내림차순이기 때문에 뒤에서부터 i-1보다 큰지만 비교하면 된다.
+        int j = n - 1;
+        while (answer[i - 1] >= answer[j]) {
+            j--;
+        }
+
+        // 3. i-1과 j의 자리를 바꿔준다.
+        int temp = answer[i - 1];
+        answer[i - 1] = answer[j];
+        answer[j] = temp;
+
+        // 4. 그 다음 i부터 끝까지 자리를 하나씩 바꿔준다.
+        j = n - 1;
+        while (i < j) {
+            temp = answer[i];
+            answer[i] = answer[j];
+            answer[j] = temp;
+            i++;
+            j--;
         }
 
         return true;
     }
-
-
 }
