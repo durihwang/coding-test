a41b49ad3cf1238475a695f6762b03ef900ef72d
황두리 <hdr@gabia.com>
2022-09-08 17:29:28 +0900

    백준 14888 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -9,47 +9,40 @@ import java.util.*;
 class Main {
 
     static int n;
-    static int[] answer;
-    static boolean[] check;
+    static int[] numbers;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            if (n == 0) {
-                break;
-            }
-            answer = new int[n];
-            check = new boolean[n];
-            for (int i = 0; i < n; i++) {
-                answer[i] = Integer.parseInt(st.nextToken());
-            }
-            go(0, 0);
-            System.out.println();
+        n = Integer.parseInt(br.readLine());
+        numbers = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int plus = Integer.parseInt(st2.nextToken());
+        int minus = Integer.parseInt(st2.nextToken());
+        int multi = Integer.parseInt(st2.nextToken());
+        int divide = Integer.parseInt(st2.nextToken());
+
+        recursive(0, plus, minus, multi, divide, numbers[0]);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static void recursive(int level, int plus, int minus, int multi, int divide, int sum) {
+        if (level == n - 1) {
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
+        } else {
+            if (plus > 0)   recursive(level + 1, plus - 1, minus, multi, divide, sum + numbers[level+1]);
+            if (minus > 0)   recursive(level + 1, plus, minus - 1, multi, divide, sum - numbers[level+1]);
+            if (multi > 0)   recursive(level + 1, plus, minus, multi - 1, divide, sum * numbers[level+1]);
+            if (divide > 0)   recursive(level + 1, plus, minus, multi, divide - 1, sum / numbers[level+1]);
         }
     }
 
-    static void go(int level, int start) {
-        if (level == 6) {
-            for (int i = 0; i < n; i++) {
-                if (check[i]) {
-                    System.out.print(answer[i] + " ");
-                }
-            }
-            /*if (check[n-1]) {
-                System.out.print(answer[n-1]);
-            }*/
-            System.out.println();
-        } else {
-            for (int i = start; i < n; i++) {
-                if (check[i]) {
-                    continue;
-                }
-                check[i] = true;
-                go(level + 1, i + 1);
-                check[i] = false;
-            }
-        }
-    }
 }
