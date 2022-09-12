a152e7360b65d49fe462593406e5ba10f36976f5
황두리 <hwangduli0515@gmail.com>
2022-09-12 23:11:48 +0900

    백준 문제풀이 - 에너지 모으기

--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,51 @@ import java.util.*;
 class Main {
 
     static int n;
-    static int[] numbers;
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
+    static int m;
+    static List<Integer> numbers;
+    static int[] check;
+    static int[] visited;
+    static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        numbers = new int[n];
+        numbers = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            numbers.add(Integer.parseInt(st.nextToken()));
         }
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int plus = Integer.parseInt(st2.nextToken());
-        int minus = Integer.parseInt(st2.nextToken());
-        int multi = Integer.parseInt(st2.nextToken());
-        int divide = Integer.parseInt(st2.nextToken());
 
-        recursive(0, plus, minus, multi, divide, numbers[0]);
+        go(0, 0);
 
-        System.out.println(max);
-        System.out.println(min);
+        System.out.println(answer);
     }
 
-    static void recursive(int level, int plus, int minus, int multi, int divide, int sum) {
-        if (level == n - 1) {
-            max = Math.max(max, sum);
-            min = Math.min(min, sum);
+    static void go(int level, int sum) {
+
+        // 첫번째와 마지막 값은 고를 수 없으니 n-2까지만 level을 돌리면 된다.
+        if (level == n - 2) {
+            // 최대값을 갱신해준다.
+            answer = Math.max(answer, sum);
         } else {
-            if (plus > 0)   recursive(level + 1, plus - 1, minus, multi, divide, sum + numbers[level+1]);
-            if (minus > 0)   recursive(level + 1, plus, minus - 1, multi, divide, sum - numbers[level+1]);
-            if (multi > 0)   recursive(level + 1, plus, minus, multi - 1, divide, sum * numbers[level+1]);
-            if (divide > 0)   recursive(level + 1, plus, minus, multi, divide - 1, sum / numbers[level+1]);
-        }
-    }
+            for (int i = 1; i < numbers.size() - 1; i++) {
 
+                // x-1 곱하기 x+1의 값을 저장한다.
+                int temp = numbers.get(i - 1) * numbers.get(i + 1);
+
+                // i의 값을 따로 저장한다.
+                int temp_index = numbers.get(i);
+
+                // i 인덱스의 값을 빼준다.
+                numbers.remove(i);
+
+                // dfs를 실행한다.
+                go(level + 1, sum + temp);
+
+                // i 인덱스에 원래 값을 다시 넣어주어 백트래킹을 해준다.
+                numbers.add(i, temp_index);
+            }
+        }
+
+    }
 }
