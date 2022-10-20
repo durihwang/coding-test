73e34131361242ee503cd3b387fd9cc623dd4d14
황두리 <hdr@gabia.com>
2022-10-20 17:42:43 +0900

    백준 1062 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -9,51 +9,54 @@ import java.util.*;
 class Main {
 
     static int n;
-    static int m;
-    static List<Integer> numbers;
-    static int[] check;
-    static int[] visited;
-    static int answer = Integer.MIN_VALUE;
+    static int k;
+    static int answer;
+    static boolean[] check;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        numbers = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        HashSet<Character> set = new HashSet<>();
+        String[] s = new String[n];
         for (int i = 0; i < n; i++) {
-            numbers.add(Integer.parseInt(st.nextToken()));
-        }
-
-        go(0, 0);
-
-        System.out.println(answer);
-    }
-
-    static void go(int level, int sum) {
-
-        // 첫번째와 마지막 값은 고를 수 없으니 n-2까지만 level을 돌리면 된다.
-        if (level == n - 2) {
-            // 최대값을 갱신해준다.
-            answer = Math.max(answer, sum);
-        } else {
-            for (int i = 1; i < numbers.size() - 1; i++) {
-
-                // x-1 곱하기 x+1의 값을 저장한다.
-                int temp = numbers.get(i - 1) * numbers.get(i + 1);
-
-                // i의 값을 따로 저장한다.
-                int temp_index = numbers.get(i);
-
-                // i 인덱스의 값을 빼준다.
-                numbers.remove(i);
-
-                // dfs를 실행한다.
-                go(level + 1, sum + temp);
-
-                // i 인덱스에 원래 값을 다시 넣어주어 백트래킹을 해준다.
-                numbers.add(i, temp_index);
+            s[i] = br.readLine();
+            char[] chars = s[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                set.add(chars[j]);
             }
         }
 
+        Character[] chars = set.toArray(new Character[0]);
+        check = new boolean[chars.length];
+
+        HashSet<Character[]> characters = new HashSet<>();
+        if (k < 5) {
+            System.out.println(0);
+        } else {
+            recursive(0, chars, characters);
+        }
     }
+
+    static void recursive(int level, Character[] chars, HashSet<Character[]> characters) {
+
+        if (level == k) {
+            // 모든 단어를 순회하면서 조건 문자 배열에 값이 다 있는지 확인
+            System.out.println(Arrays.toString(check));
+        } else {
+            for (int i = 0; i < chars.length; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    recursive(level + 1, chars, characters);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+
+    static void checkWord() {
+
+    }
+
 }
