f0231cf37587bdd4ffcc7865435aa8dcdaf8bedd
황두리 <hwangduli0515@gmail.com>
2022-10-21 11:02:02 +0900

    백준 문제풀이 - 1062 가르침

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ class Main {
     static int n;
     static int k;
     static int answer;
+    static String[] s;
     static boolean[] check;
     public static void main(String[] args) throws IOException {
 
@@ -18,45 +19,62 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        s = new String[n];
+        answer = Integer.MIN_VALUE;
         HashSet<Character> set = new HashSet<>();
-        String[] s = new String[n];
         for (int i = 0; i < n; i++) {
-            s[i] = br.readLine();
-            char[] chars = s[i].toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                set.add(chars[j]);
-            }
+            String word = br.readLine().replace("anta", "").replace("tica", "");
+            s[i] = word;
         }
 
-        Character[] chars = set.toArray(new Character[0]);
-        check = new boolean[chars.length];
-
-        HashSet<Character[]> characters = new HashSet<>();
         if (k < 5) {
             System.out.println(0);
-        } else {
-            recursive(0, chars, characters);
+            System.exit(0);
+        } else if (k > 25) {
+            System.out.println(n);
+            System.exit(0);
         }
+
+        check = new boolean[26];
+        check['a' - 'a'] = true;
+        check['c' - 'a'] = true;
+        check['i' - 'a'] = true;
+        check['n' - 'a'] = true;
+        check['t' - 'a'] = true;
+
+        recursive(0, 0);
+
+        System.out.println(answer);
     }
 
-    static void recursive(int level, Character[] chars, HashSet<Character[]> characters) {
+    static void recursive(int level, int index) {
 
-        if (level == k) {
+        if (level == k - 5) {
             // 모든 단어를 순회하면서 조건 문자 배열에 값이 다 있는지 확인
-            System.out.println(Arrays.toString(check));
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                boolean read = true;
+                for (int j = 0; j < s[i].length(); j++) {
+                    if (!check[s[i].charAt(j) - 'a']) {
+                        read = false;
+                        break;
+                    }
+                }
+                if (read) {
+                    count++;
+                }
+            }
+            answer = Math.max(answer, count);
         } else {
-            for (int i = 0; i < chars.length; i++) {
+            for (int i = index; i < 26; i++) {
                 if (!check[i]) {
                     check[i] = true;
-                    recursive(level + 1, chars, characters);
+                    recursive(level + 1, i);
                     check[i] = false;
                 }
             }
         }
     }
 
-    static void checkWord() {
-
-    }
 
 }
