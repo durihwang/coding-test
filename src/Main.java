f8a3eda684d818e89189c2ba94e6a1d6222cd226
황두리 <hdr@gabia.com>
2022-10-25 18:03:39 +0900

    백준 1038 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -11,70 +11,42 @@ class Main {
     static int n;
     static int k;
     static int answer;
-    static String[] s;
-    static boolean[] check;
+    static boolean check;
+    static ArrayList<Long> reduction = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        s = new String[n];
-        answer = Integer.MIN_VALUE;
-        HashSet<Character> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            String word = br.readLine().replace("anta", "").replace("tica", "");
-            s[i] = word;
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 10; i++) {
+            recursive(1, i);
         }
 
-        if (k < 5) {
-            System.out.println(0);
-            System.exit(0);
-        } else if (k > 25) {
+        System.out.println(reduction.size());
+        Collections.sort(reduction);
+
+        if (n < 10) {
             System.out.println(n);
-            System.exit(0);
-        }
-
-        check = new boolean[26];
-        check['a' - 'a'] = true;
-        check['c' - 'a'] = true;
-        check['i' - 'a'] = true;
-        check['n' - 'a'] = true;
-        check['t' - 'a'] = true;
-
-        recursive(0, 0);
-
-        System.out.println(answer);
-    }
-
-    static void recursive(int level, int index) {
-
-        if (level == k - 5) {
-            // 모든 단어를 순회하면서 조건 문자 배열에 값이 다 있는지 확인
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                boolean read = true;
-                for (int j = 0; j < s[i].length(); j++) {
-                    if (!check[s[i].charAt(j) - 'a']) {
-                        read = false;
-                        break;
-                    }
-                }
-                if (read) {
-                    count++;
-                }
-            }
-            answer = Math.max(answer, count);
         } else {
-            for (int i = index; i < 26; i++) {
-                if (!check[i]) {
-                    check[i] = true;
-                    recursive(level + 1, i);
-                    check[i] = false;
-                }
+            if (reduction.size() <= n) {
+                System.out.println(-1);
+            } else {
+                System.out.println(reduction.get(n));
             }
         }
     }
 
+    public static void recursive(int level, long number) {
 
+        if (level > 10) {
+            return;
+        }
+
+        reduction.add(number);
+
+        for (int i = 0; i < number % 10; i++) {
+            recursive(level + 1, (number * 10) + i);
+        }
+    }
 }
