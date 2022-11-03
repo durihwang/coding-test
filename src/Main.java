6edb7f7f838be977c4d52260a3733efd122c3e9b
황두리 <hwangduli0515@gmail.com>
2022-11-03 23:28:10 +0900

    백준 문제풀이 - 쉬운 계단수

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,42 @@ import java.util.*;
 class Main {
 
     static int n;
-    static int k;
-    static int answer;
-    static boolean check;
-    static int[] d;
-    static int[] p;
-    static ArrayList<Long> reduction = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        d = new int[n + 1];
-        p = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+        int mod = 1000000000;
+        int answer = 0;
+
+        int[][] d = new int[101][10];
+        for (int i = 1; i < 10; i++) {
+            d[1][i] = 1;
         }
 
-        d[0] = 0;
-
-        // 1부터 n번째 까지의 카드팩의 가격 모두 구한다.
-        for (int i = 1; i <= n; i++) {
-
-            // 1은 1번, 2는 2번 ... n은 n번 카드를 얻을 수 있다.
-            for (int j = 1; j <= i; j++) {
-                d[i] = Math.max(d[i], d[i - j] + p[j]);
-                System.out.println(Arrays.toString(d));
+        //
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j > 0) {
+                    d[i][j] += d[i - 1][j - 1];
+                }
+                if (j < 9) {
+                    d[i][j] += d[i - 1][j + 1];
+                }
+                d[i][j] %= mod;
             }
-
         }
 
-        System.out.println(d[n]);
+        /*for (int[] s : d) {
+            System.out.println(Arrays.toString(s));
+        }*/
 
+        // 점화식
+        for (int i = 0; i <= 9; i++) {
+            answer += d[n][i];
+        }
+
+        System.out.println(answer % mod);
     }
 
 }
