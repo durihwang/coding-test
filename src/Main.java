38814806c0a5d86bc468c5c34fee1a7b19180fe4
황두리 <hwangduli0515@gmail.com>
2022-11-06 22:51:18 +0900

    백준 문제풀이 - 가장 긴 증가하는 부분 수열 4

--- src/Main.java
+++ src/Main.java
@@ -9,62 +9,56 @@ import java.util.*;
 class Main {
 
     static int n;
+    static int[] v;
+    static long[] a;
+    static ArrayList<Long> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int max = 0;
-        int[] test = new int[n];
+        a = new long[n];
+        long[] d = new long[n];
+        v = new int[n];
+
+        long answer = 0;
+        int p = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            test[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, test[i]);
-        }
-
-        long mod = 1000000009L;
-        long answer;
-
-        long[][] d = new long[1000001][4];
-
-        for (int i = 0; i <= max; i++) {
-
-            if (i - 1 >= 0) {
-                d[i][1] += d[i - 1][1] + d[i - 1][2] + d[i - 1][3];
-                if (i == 1) {
-                    d[i][1] = 1;
-                }
-            }
-
-            if (i - 1 >= 1) {
-                d[i][2] = d[i - 2][1] + d[i - 2][2] + d[i - 2][3];
-                if (i == 2) {
-                    d[i][2] = 1;
-                }
-            }
-
-            if (i - 1 >= 2) {
-                d[i][3] = d[i - 3][1] + d[i - 3][2] + d[i - 3][3];
-                if (i == 3) {
-                    d[i][3] = 1;
-                }
-            }
-
-            d[i][1] %= mod;
-            d[i][2] %= mod;
-            d[i][3] %= mod;
-        }
-
-
-        for (int i = 0; i < 6; i++) {
-//            System.out.println(Arrays.toString(d[i]));
+            a[i] = Integer.parseInt(st.nextToken());
+            d[i] = 1;
+            v[i] = -1;
         }
 
         for (int i = 0; i < n; i++) {
-            answer = d[test[i]][1] + d[test[i]][2] + d[test[i]][3];
-            System.out.println(answer % mod);
+            for (int j = i; j >= 0; j--) {
+                if (d[j] + 1 > d[i] && a[i] > a[j]) {
+                    d[i] = d[j] + 1;
+                    v[i] = j;
+                }
+            }
         }
 
+        for (int i = 0; i < d.length; i++) {
+            if (d[i] > answer) {
+                answer = d[i];
+                p = i;
+            }
+        }
 
+        System.out.println(answer);
+        System.out.println(Arrays.toString(v));
+        go(p);
+    }
+
+    static void go(int p) {
+
+        if (p == -1) {
+            return;
+        }
+
+        go(v[p]);
+        System.out.print(a[p] + " ");
     }
 
 }
