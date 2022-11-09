e53ec718b6df2bfd77e4047b6a9c8234e3a33eb8
황두리 <hdr@gabia.com>
2022-11-09 18:01:29 +0900

    백준 - 유기농 배추 풀이

--- src/Main.java
+++ src/Main.java
@@ -9,54 +9,93 @@ import java.util.*;
 class Main {
 
     static int n;
+    static int[] size;
+    static int[][] map;
+    static boolean[][] visited;
 
+    // 상 하 좌 우 확인
+    static int[] location_x = {0, 0, -1, 1};
+    static int[] location_y = {1, -1, 0, 0};
+    static Queue<Node> queue = new LinkedList<>();
+
+    static class Node {
+        private int x;
+        private int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int max = 0;
-        int[] test = new int[n];
+        size = new int[3];
         for (int i = 0; i < n; i++) {
-            test[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, test[i]);
-        }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                size[j] = Integer.parseInt(st.nextToken());
+            }
 
-        long mod = 1000000009L;
-        long answer;
+            int answer = 0;
+            map = new int[size[0]][size[1]];
+            visited = new boolean[size[0]][size[1]];
 
-        long[][] d = new long[100001][4];
+            for (int j = 0; j < size[2]; j++) {
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                int i1 = Integer.parseInt(st2.nextToken());
+                int j1 = Integer.parseInt(st2.nextToken());
+                map[i1][j1] = 1;
+            }
 
-        for (int i = 1; i <= max; i++) {
-            if (i - 1 >= 0) {
-                d[i][1] = d[i - 1][2] + d[i - 1][3];
-                if (i == 1) {
-                    d[i][1] = 1;
+            for (int j = 0; j < size[0]; j++) {
+                for (int k = 0; k < size[1]; k++) {
+                    if (!visited[j][k] && map[j][k] == 1) {
+                        // dfs 혹은 bfs 시작
+                        answer++;
+                        bfs(j, k);
+                    }
                 }
             }
 
-            if (i - 1 >= 1) {
-                d[i][2] = d[i - 2][1] + d[i - 2][3];
-                if (i == 2) {
-                    d[i][2] = 1;
+            System.out.println(answer);
+        }
+    }
+
+    public static void bfs(int x, int y) {
+
+        visited[x][y] = true;
+        queue.offer(new Node(x, y));
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int xx = node.x + location_x[i];
+                int yy = node.y + location_y[i];
+                if (location_check(xx, yy) && !visited[xx][yy] && map[xx][yy] == 1) {
+                    visited[xx][yy] = true;
+                    queue.offer(new Node(xx, yy));
                 }
             }
-
-            if (i - 1 >= 2) {
-                d[i][3] = d[i - 3][1] + d[i - 3][2];
-                if (i == 3) {
-                    d[i][3] = 1;
-                }
-            }
-
-            d[i][1] %= mod;
-            d[i][2] %= mod;
-            d[i][3] %= mod;
         }
+    }
 
-        for (int i = 0; i < n; i++) {
-            answer = d[test[i]][1] + d[test[i]][2] + d[test[i]][3];
-            System.out.println(answer % mod);
-        }
+    public static void dfs() {
+
+    }
+
+    public static boolean location_check(int x, int y) {
+        return x >= 0 && x < size[0] && y >= 0 && y < size[1];
     }
 
 }
