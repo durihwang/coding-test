34a1a95211910af737bc416254dcb411864a6de7
황두리 <hwangduli0515@gmail.com>
2022-11-12 01:54:24 +0900

    백준 문제풀이 - 제곱수의 합

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,19 @@ class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] d = new int[n + 1];
 
-        int[] a = new int[n];
-        int[][] d = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            d[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                if (d[i] > d[i - j * j] + 1) {
+                    d[i] = d[i - j * j] + 1;
+                }
+            }
         }
 
-        // 0은 제거하지 않는 경우 -> 기존대로 연속합의 최대값을 저장해준다.
-        // 1은 제거하는 경우 -> 현재 수를 제거하는 경우와 제거하지 않는 경우로 나누어진다.
-        d[0][0] = d[0][1] = a[0];
-        int answer = a[0];
-
-        for (int i = 1; i < n; i++) {
-            d[i][0] = Math.max(d[i - 1][0] + a[i], a[i]);
-            d[i][1] = Math.max(d[i - 1][0], d[i - 1][1] + a[i]);
-            answer = Math.max(answer, Math.max(d[i][0], d[i][1]));
-        }
-
-        System.out.println(answer);
-
+        System.out.println(d[n]);
     }
+
 
 }
