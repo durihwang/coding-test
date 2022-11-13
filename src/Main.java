cc1a0242b6b8dcf64d12df5ddd95f7000505d363
hwangduli <hwangduli0515@gmail.com>
2022-11-13 23:10:38 +0900

    백준 2225 - 합분해 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,32 @@ import java.util.*;
 class Main {
 
     static int n;
+    static int k;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        int[] d = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            d[i] = i;
-            for (int j = 1; j * j <= i; j++) {
-                if (d[i] > d[i - j * j] + 1) {
-                    d[i] = d[i - j * j] + 1;
-                }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int mod = 1000000000;
+        int[][] d = new int[k + 1][n + 1];
+        Arrays.fill(d[1], 1);
+
+        for (int i = 1; i <= k; i++) d[i][0] = 1;
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                d[i][j] = d[i - 1][j] + d[i][j - 1];
+                d[i][j] %= mod;
             }
         }
 
-        System.out.println(d[n]);
+        System.out.println(d[k][n]);
+
     }
+
 
 
 }
