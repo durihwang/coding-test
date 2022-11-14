cd8de90f7d344c922219f26a6be31e1e71532705
hwangduli <hwangduli0515@gmail.com>
2022-11-14 23:25:14 +0900

    RGB 거리 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,36 @@ import java.util.*;
 class Main {
 
     static int n;
-    static int k;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        int mod = 1000000000;
-        int[][] d = new int[k + 1][n + 1];
-        Arrays.fill(d[1], 1);
+        n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[][] r = new int[n + 1][3];
+        int[][] d = new int[n + 1][3];
 
-        for (int i = 1; i <= k; i++) d[i][0] = 1;
-
-        for (int i = 2; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                d[i][j] = d[i - 1][j] + d[i][j - 1];
-                d[i][j] %= mod;
-            }
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            r[i][0] = Integer.parseInt(st.nextToken());
+            r[i][1] = Integer.parseInt(st.nextToken());
+            r[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(d[k][n]);
+        d[1][0] = r[1][0];
+        d[1][1] = r[1][1];
+        d[1][2] = r[1][2];
+
+        for (int i = 2; i <= n; i++) {
+            d[i][0] = Math.min(d[i - 1][1], d[i - 1][2]) + r[i][0];
+            d[i][1] = Math.min(d[i - 1][0], d[i - 1][2]) + r[i][1];
+            d[i][2] = Math.min(d[i - 1][0], d[i - 1][1]) + r[i][2];
+        }
+
+        answer = Math.min(d[n][0], d[n][1]);
+        answer = Math.min(answer, d[n][2]);
+        System.out.println(answer);
 
     }
 
