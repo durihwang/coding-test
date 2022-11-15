8b664b2ca1b9413a0943dc1eb0470b280b070f48
hwangduli <hwangduli0515@gmail.com>
2022-11-15 21:25:12 +0900

    백준 - 동물원 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -15,30 +15,27 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
+        int mod = 9901;
         int answer = 0;
-        int[][] r = new int[n + 1][3];
         int[][] d = new int[n + 1][3];
 
+        Arrays.fill(d[0], 1);
+
         for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            r[i][0] = Integer.parseInt(st.nextToken());
-            r[i][1] = Integer.parseInt(st.nextToken());
-            r[i][2] = Integer.parseInt(st.nextToken());
+            d[i][0] = d[i - 1][0] + d[i - 1][1] + d[i - 1][2];
+            d[i][1] = d[i - 1][0] + d[i - 1][2];
+            d[i][2] = d[i - 1][0] + d[i - 1][1];
+
+            d[i][0] %= mod;
+            d[i][1] %= mod;
+            d[i][2] %= mod;
+
         }
 
-        d[1][0] = r[1][0];
-        d[1][1] = r[1][1];
-        d[1][2] = r[1][2];
+        answer = d[n - 1][0] + d[n - 1][1] + d[n - 1][2];
 
-        for (int i = 2; i <= n; i++) {
-            d[i][0] = Math.min(d[i - 1][1], d[i - 1][2]) + r[i][0];
-            d[i][1] = Math.min(d[i - 1][0], d[i - 1][2]) + r[i][1];
-            d[i][2] = Math.min(d[i - 1][0], d[i - 1][1]) + r[i][2];
-        }
+        System.out.println(answer % mod);
 
-        answer = Math.min(d[n][0], d[n][1]);
-        answer = Math.min(answer, d[n][2]);
-        System.out.println(answer);
 
     }
 
