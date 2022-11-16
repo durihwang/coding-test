ee4a53119cf2085fcf2639229dd37c5465ee0fc0
hwangduli <hwangduli0515@gmail.com>
2022-11-17 00:24:06 +0900

    백준 - 1,2,3 더하기 4 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -15,26 +15,40 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        int mod = 9901;
-        int answer = 0;
-        int[][] d = new int[n + 1][3];
-
-        Arrays.fill(d[0], 1);
-
-        for (int i = 1; i <= n; i++) {
-            d[i][0] = d[i - 1][0] + d[i - 1][1] + d[i - 1][2];
-            d[i][1] = d[i - 1][0] + d[i - 1][2];
-            d[i][2] = d[i - 1][0] + d[i - 1][1];
-
-            d[i][0] %= mod;
-            d[i][1] %= mod;
-            d[i][2] %= mod;
-
+        int[][] d = new int[10001][3];
+        int[] number = new int[n];
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(br.readLine());
+        }
+        // 1을 이용해서 n을 만드는 경우는 무조건 1개이다.
+        for (int i = 1; i <= 10000; i++) {
+            d[i][0] = 1;
         }
 
-        answer = d[n - 1][0] + d[n - 1][1] + d[n - 1][2];
+        for (int i = 0; i <= 10000; i++) {
+            if (i - 1 >= 0) {
+                d[i][0] = d[i - 1][0];
+                if (i == 1) {
+                    d[i][0] = 1;
+                }
+            }
+            if (i - 1 >= 1) {
+                d[i][1] = d[i - 2][0] + d[i - 2][1];
+                if (i == 2) {
+                    d[i][1] = 1;
+                }
+            }
+            if (i - 1 >= 2) {
+                d[i][2] = d[i - 3][0] + d[i - 3][1] + d[i - 3][2];
+                if (i == 3) {
+                    d[i][2] = 1;
+                }
+            }
+        }
 
-        System.out.println(answer % mod);
+        for (int i = 0; i < n; i++) {
+            System.out.println(d[number[i]][0] + d[number[i]][1] + d[number[i]][2]);
+        }
 
 
     }
