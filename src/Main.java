e7e2d4a39c0b7c67e37df44cd34547b9c3b01c69
hwangduli <hwangduli0515@gmail.com>
2022-11-21 23:51:01 +0900

    백준 - 가장 긴 바이토닉 부분 수열 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -15,41 +15,40 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        int[][] d = new int[10001][3];
-        int[] number = new int[n];
+        int[] a = new int[n];
+        int[] d_plus = new int[n];
+        int[] d_minus = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            number[i] = Integer.parseInt(br.readLine());
-        }
-        // 1을 이용해서 n을 만드는 경우는 무조건 1개이다.
-        for (int i = 1; i <= 10000; i++) {
-            d[i][0] = 1;
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i <= 10000; i++) {
-            if (i - 1 >= 0) {
-                d[i][0] = d[i - 1][0];
-                if (i == 1) {
-                    d[i][0] = 1;
-                }
-            }
-            if (i - 1 >= 1) {
-                d[i][1] = d[i - 2][0] + d[i - 2][1];
-                if (i == 2) {
-                    d[i][1] = 1;
-                }
-            }
-            if (i - 1 >= 2) {
-                d[i][2] = d[i - 3][0] + d[i - 3][1] + d[i - 3][2];
-                if (i == 3) {
-                    d[i][2] = 1;
+        Arrays.fill(d_plus, 1);
+        Arrays.fill(d_minus, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[i] > a[j]) {
+                    d_plus[i] = Math.max(d_plus[i], d_plus[j] + 1);
                 }
             }
         }
 
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    d_minus[i] = Math.max(d_minus[i], d_minus[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
         for (int i = 0; i < n; i++) {
-            System.out.println(d[number[i]][0] + d[number[i]][1] + d[number[i]][2]);
+
+            answer = Math.max(answer, d_plus[i] + d_minus[i] - 1);
         }
 
+        System.out.println(answer);
 
     }
 
