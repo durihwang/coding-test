128364bb45d1db4b2a8c44ae8db08ad5927f533b
황두리 <hdr@gabia.com>
2022-11-24 17:45:36 +0900

    백준 - abcde 풀이

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,10 @@ class Main {
     static int n;
     static int m;
 
-    class Edge {
+    static class Edge {
+
         int from, to;
+
         Edge(int from, int to) {
             this.from = from;
             this.to = to;
@@ -23,31 +25,55 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        ArrayList<Integer>[] g = new ArrayList[n];
+        boolean[][] a = new boolean[n][n];
+        ArrayList<Integer>[] g = (ArrayList<Integer>[]) new ArrayList[n];
         ArrayList<Edge> edges = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            g[i] = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            g[i] = new ArrayList<Integer>();
         }
 
         for (int i = 0; i < m; i++) {
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st2.nextToken());
             int to = Integer.parseInt(st2.nextToken());
+
+            // 양방향 모두 친구가 가능하므로 양방향 모두 등록해준다.
+            edges.add(new Edge(to, from));
+            edges.add(new Edge(from, to));
+            a[from][to] = a[to][from] = true;
             g[from].add(to);
             g[to].add(from);
         }
 
+        // 양방향 모두 확인하기 위해서 두배로 늘려줌
         m *= 2;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
-                int A = g[i].get(j);
+                int A = edges.get(i).to;
+                int B = edges.get(i).from;
+                int C = edges.get(j).to;
+                int D = edges.get(j).from;
+
+                if (A == B || A == C || A == D || B == C || B == D || C == D) {
+                    continue;
+                }
+
+                if (!a[B][C]) continue;
+
+                for (int E : g[D]) {
+                    if (A == E || B == E || C == E || D == E) {
+                        continue;
+                    }
+                    System.out.println(1);
+                    System.exit(0);
+                }
             }
         }
 
+        System.out.println("0");
     }
-
 
 
 }
