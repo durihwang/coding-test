ae19346645b24a40e1b8afdcde50387e219bf758
hwangduli <hwangduli0515@gmail.com>
2022-11-28 23:03:58 +0900

    코딜리티 연습 문제 풀이

--- src/Solution.java
+++ src/Solution.java
@@ -8,24 +8,32 @@ import java.util.List;
 public class Solution {
 
     public void solution(int N) {
-        int answer = 0 ;
-        /*while (N > 0) {
-            if (enable_print == 0 && N % 10 != 0) {
-                enable_print = 1;
-            }
+        int answer = 0;
+        int enable_print;
 
-            else if (enable_print == 1) {
+        // 나머지가 0이면 0, 아니면 1로 초기화
+        if (N % 10 == 0) {
+            enable_print = 0;
+        } else {
+            enable_print = 1;
+        }
+
+        while (N > 0) {
+            if (enable_print == 0) {
+                if (N % 10 != 0) {
+                    enable_print = 1;
+                    System.out.print(N % 10);
+                }
+            } else {
                 System.out.print(N % 10);
             }
             N = N / 10;
-        }*/
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
-        int[] A = {1, 2, 4, 4, 3};
-        int[] B = {2, 3, 1, 3, 1};
-//        System.out.println(solution.solution(4, A, B));
+        solution.solution(1000010000);
     }
 }
