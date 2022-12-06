84e65fd587ccff161ce4ea8348d4085c3500c7a6
hwangduli <hwangduli0515@gmail.com>
2022-12-06 22:57:07 +0900

    나이트 이동 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,88 @@ import java.util.*;
 
 class Main {
 
+    static int t;
     static int n;
+    static int m;
+    static int[] dx = {-1, 1, 2, 2, 1, -1, -2, -2};
+    static int[] dy = {-2, -2, -1, 1, 2, 2, 1, -1};
+
+    static class Node {
+        private int x;
+        private int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        t = Integer.parseInt(br.readLine());
 
-        long[] d = new long[n + 1];
-        d[0] = 1;
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] map = new int[n][n];
+            int[][] dist = new int[n][n];
+            int[][] visited = new int[n][n];
 
-        for (int i = 2; i <= n; i += 2) {
-            d[i] = 3 * d[i - 2];
-            for (int j = i - 4; j >= 0; j -= 2) {
-                d[i] += d[j] * 2;
+            Queue<Node> q = new LinkedList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            q.add(new Node(x, y));
+            visited[x][y] = 1;
+
+            while (!q.isEmpty()) {
+                Node poll = q.poll();
+                int xx = poll.x;
+                int yy = poll.y;
+
+                for (int j = 0; j < 8; j++) {
+                    int xxx = xx + dx[j];
+                    int yyy = yy + dy[j];
+                    if (xxx >= 0 && yyy >= 0 && xxx < n && yyy < n) {
+                        if (visited[xxx][yyy] == 0) {
+                            dist[xxx][yyy] = dist[xx][yy] + 1;
+                            visited[xxx][yyy] = 1;
+                            q.add(new Node(xxx, yyy));
+                        }
+                    }
+                }
             }
 
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int location_x = Integer.parseInt(st2.nextToken());
+            int location_y = Integer.parseInt(st2.nextToken());
+
+            System.out.println(dist[location_x][location_y]);
         }
 
-//        System.out.println(Arrays.toString(d));
-        System.out.println(d[n]);
+    }
 
+    static void bfs(int[][] map, int[][] dist, int[][] visited, int x, int y) {
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
+        visited[x][y] = 1;
+        dist[x][y] = dist[x][y] + 1;
+
+        while (!q.isEmpty()) {
+            Node poll = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int xx = poll.x + dx[i];
+                int yy = poll.y + dy[i];
+                if (xx >= 0 && yy >= 0 && xx < n && yy < m) {
+                    if (map[xx][yy] == 1 && visited[xx][yy] == 0) {
+                        q.add(new Node(xx, yy));
+                        dist[xx][yy] = dist[poll.x][poll.y] + 1;
+                        visited[xx][yy] = 1;
+                    }
+                }
+            }
+        }
 
     }
 
