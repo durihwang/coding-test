713005d64fe5d0850250a06209bd67e86a10ec20
hwangduli <hwangduli0515@gmail.com>
2022-12-12 22:40:34 +0900

    숨바꼭질 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -11,84 +11,72 @@ class Main {
     static int[] dx = {-1, 1, 2, 2, 1, -1, -2, -2};
     static int[] dy = {-2, -2, -1, 1, 2, 2, 1, -1};
 
-    static class Node {
-        private int x;
-        private int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int max = 200000;
+        boolean[] check = new boolean[max];
+        int[] dist = new int[max];
+        int[] from = new int[max];
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[][] map = new int[n][n];
-            int[][] dist = new int[n][n];
-            int[][] visited = new int[n][n];
-
-            Queue<Node> q = new LinkedList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            q.add(new Node(x, y));
-            visited[x][y] = 1;
-
-            while (!q.isEmpty()) {
-                Node poll = q.poll();
-                int xx = poll.x;
-                int yy = poll.y;
-
-                for (int j = 0; j < 8; j++) {
-                    int xxx = xx + dx[j];
-                    int yyy = yy + dy[j];
-                    if (xxx >= 0 && yyy >= 0 && xxx < n && yyy < n) {
-                        if (visited[xxx][yyy] == 0) {
-                            dist[xxx][yyy] = dist[xx][yy] + 1;
-                            visited[xxx][yyy] = 1;
-                            q.add(new Node(xxx, yyy));
-                        }
-                    }
-                }
-            }
-
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int location_x = Integer.parseInt(st2.nextToken());
-            int location_y = Integer.parseInt(st2.nextToken());
-
-            System.out.println(dist[location_x][location_y]);
-        }
-
-    }
-
-    static void bfs(int[][] map, int[][] dist, int[][] visited, int x, int y) {
-
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y));
-        visited[x][y] = 1;
-        dist[x][y] = dist[x][y] + 1;
+        LinkedList<Integer> q = new LinkedList<>();
+        q.add(n);
+        check[n] = true;
+        dist[n] = 0;
 
         while (!q.isEmpty()) {
-            Node poll = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int xx = poll.x + dx[i];
-                int yy = poll.y + dy[i];
-                if (xx >= 0 && yy >= 0 && xx < n && yy < m) {
-                    if (map[xx][yy] == 1 && visited[xx][yy] == 0) {
-                        q.add(new Node(xx, yy));
-                        dist[xx][yy] = dist[poll.x][poll.y] + 1;
-                        visited[xx][yy] = 1;
-                    }
+            Integer now = q.poll();
+
+            // x-1
+            if (now - 1 >= 0) {
+                if (!check[now - 1]) {
+                    check[now - 1] = true;
+                    q.add(now - 1);
+                    from[now - 1] = now;
+                    dist[now - 1] = dist[now] + 1;
+                }
+            }
+
+            // x+1
+            if (now + 1 < max) {
+                if (!check[now + 1]) {
+                    check[now + 1] = true;
+                    q.add(now + 1);
+                    from[now + 1] = now;
+                    dist[now + 1] = dist[now] + 1;
+                }
+            }
+
+            // 2*x
+            if (now * 2 < max) {
+                if (!check[now * 2]) {
+                    check[now * 2] = true;
+                    q.add(now * 2);
+                    from[now * 2] = now;
+                    dist[now * 2] = dist[now] + 1;
                 }
             }
         }
 
+        System.out.println(dist[m]);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = m; i != n; i = from[i]) {
+            list.add(i);
+        }
+
+        list.add(n);
+
+        for (int i = list.size(); i > 0; i--) {
+            System.out.print(list.get(i-1) + " ");
+        }
+
+
     }
+
 
 
 }
